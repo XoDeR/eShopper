4d3a4997d0c1ff29d6609d8a8c20cd081cf8fa19
Volodymyr <XoDeR@users.noreply.github.com>
2023-10-20 21:11:31 +0200

    Added empty register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useAppSelector } from "./hooks/hooks";
 import { RootState } from "./redux/store";
 import "./App.css";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 function App() {
-  const userId = useSelector((state: RootState) => state.user.currentUserId);
+  const userId = useAppSelector((state: RootState) => state.user.currentUserId);
   return (
     <div>
       <Routes>
         <Route path="/" element={userId !== "" ? <Home /> : <Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
